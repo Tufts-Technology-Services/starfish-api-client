abe1a5c30648e52d2301cec2cb8f740decfa2dad
Chris Barnett <Christopher.Barnett@tufts.edu>
2026-05-01 19:22:41 -0400

    cleanup

--- starfish_api_client/redash_api_client.py
+++ starfish_api_client/redash_api_client.py
@@ -23,21 +23,3 @@ class RedashAPIClient(AbstractClient):
         """
         self._download_file(f'queries/{self.query_id}/results.csv', f'{local_filename}.csv')
     
-    def _download_file(self, endpoint: str, local_filename: str, chunk_size: int = 524_288):
-        """
-        downloads a file from a url, with options for setting headers, parameters, and chunk size to tune performance
-        :param url: remote file location
-        :param local_filename: local file location
-        :param params: url query parameters
-        :param headers: http headers
-        :param chunk_size:
-        :return:
-        """
-        headers = {
-            'Authorization': f'Key {self.token}'
-        }
-        with requests.get(os.path.join(self.url, endpoint), headers=headers, stream=True) as r:
-            r.raise_for_status()
-            with open(local_filename, 'wb') as f:
-                for chunk in r.iter_content(chunk_size=chunk_size):
-                    f.write(chunk)
